be651e716c54f95b88052fded4b7bd0129607b6d
Nick Sanford <nsanford46@gmail.com>
2019-10-16 14:48:12 -0500

    yeah

---
+++ f19/algorithms/phone-numbers.py
@@ -0,0 +1,10 @@
+import sys
+
+keypad = { 
+    '1':'1', '2':'2abc', '3':'3def', '4':'4ghi', '5':'5jkl', '6':'6mno', '7':'7pqrs', '8':'8tuv', '9':'9wxyz', '0':'0'
+
+}
+
+def phone_words(number):
+    return 0
+    
